13dcfe67c47afc622f60f6c275d1fa990e3f0e4d
Jamile Portela <portela.jamile@gmail.com>
2016-10-08 21:44:16 -0300

    adc parte inicial das fcs attr e add

--- int128.c
+++ int128.c
@@ -11,12 +11,35 @@ long getLow (Int128 *a)
 /*Funções principais*/
 void int128_attr (Int128 *res, long l)
 {
-	
+		if(l>0){
+		res->high =0;
+		res->low = l;	
+	}
+	else {
+		res->high =-1;
+		res->low = l;	
+	}
+		
 }
 
 void int128_add (Int128 *res, Int128 *v1, Int128 *v2)
 {
-
+	if(abs(v1->low+v2->low)>4294967295) //há overflow
+	{
+		// descobrir o q fazer aq
+		
+		
+	}
+	else{
+		res->low=v1->low+v2->low;
+		if( (v1->high == -1 || v1->high==0)&& (v12->high == -1 || v2->high==0)) 
+			// se o high apenas representa o sinal , a resposta vai ter o sinal do maior
+			res->high=(v1->low>v2->low)? v1->high:v2->high; 
+		else 
+			
+		if(v1->high ==0 && v2->high ==0)
+			
+	}
 }
 
 void int128_sub (Int128 *res, Int128 *v1, Int128 *v2)
