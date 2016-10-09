3d7da1fbe8f28e62462876fae82af7335b0349a7
Jamile Portela <portela.jamile@gmail.com>
2016-10-09 18:53:27 -0300

    organiza arq de teste

--- tstJ.c
+++ tstJ.c
@@ -1,6 +1,6 @@
 #include "int128.h"
 #include <stdio.h>
-/*
+/* REMOVER DEPOIS
 void int128_attr 
 void int128_add 
 void int128_sub 
@@ -10,6 +10,7 @@ int int128_write
 int int128_read
 */
 
+// FCS AUXILIARES P TESTE
 void dump (void *p, int n)
  {
   unsigned char *p1 = p;
@@ -18,24 +19,46 @@ void dump (void *p, int n)
     p1++;
   }
 }
- int main (void)
+ void exibeInt( Int128* b)
  {
-	long a,c,d;
-	Int128 b;
-	a=-2147483647;
-	
-	int128_attr(&b,a);
-	
-	c=getHigh(&b);
-	d=getLow(&b);	
-	
-	printf("a - %ld - dump de a:\n",a );
-	dump(&a,sizeof(a));
+	long c,d;
+	c=getHigh(b);
+	d=getLow(b);	
 
 	printf("HIGH long: %ld - dump:\n",c );
 	dump(&c,sizeof(c));
 	printf("LOW long: %ld - dump:\n",d );
 	dump(&d,sizeof(d));
+	 
+ }
+ void exibeLong (long a)
+ {
+	 printf("num - %ld - dump de num:\n",a );
+	dump(&a,sizeof(a));
+ }
+ 
+ int main (void)
+ {
+	long a,b;
+	Int128 i1,i2,i3;
+	a=-2147483648;
+	b= 2147483646;
+	int128_attr(&i1,a);
+	int128_attr(&i2,b);
+	
+	/* TESTA A FC ATTR	
+	exibeLong(a);
+	exibeInt(&i1);
+	*/
+	
+	// TESTA A FC ADD 
+	int128_add(&i3,&i1,&i2);
+	exibeInt(&i1);
+	exibeInt(&i2);
+	exibeLong (a+b);
+	exibeInt(&i3);
+	
+	
 	
  	return 0;
  }
